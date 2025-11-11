ca4a6c8df9c8791766db58878873ae1cf8d66e19
Вячеслав Дорошенко <44668361+SlavaDoroshenko@users.noreply.github.com>
2025-11-11 19:17:41 +0300

    test https

--- src/pages/main.jsx
+++ src/pages/main.jsx
@@ -21,7 +21,7 @@ const Main = () => (
       <Container className="p-4 bg-[#111c27]">
         <Flex direction="column" align="center">
           <Typography.Headline variant="large-strong">
-            Дневник
+            МАХ ДЛЯ МОЛОДЦОВ
           </Typography.Headline>
 
           <Flex direction="row" align="center" gap={72}>
