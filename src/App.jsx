25cbeae4a3ce1c68c9cb6ee19819cb8f55323f3f
SlavaDoroshenko <lblue1256@gmail.com>
2025-11-13 16:22:24 +0300

    test new barcode library

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,13 @@
 import { Route, Routes } from "react-router-dom";
 import Test from "./pages/test-max-bridge";
 import Main from "./pages/main";
-import BarcodeScannerPage from "./pages/barcode-scanner";
+import BarcodeScanner from "./pages/barcode-scanner";
 
 const App = () => (
   <Routes>
     <Route path="/" element={<Main />} />
     <Route path="/test" element={<Test />} />
-    <Route path="/barcode" element={<BarcodeScannerPage />} />
+    <Route path="/barcode" element={<BarcodeScanner />} />
   </Routes>
 );
 
